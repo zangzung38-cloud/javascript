df57d1c999eef006840f5803bf9725171c487995
zang38 <zangzung38@gmail.com>
2025-10-25 19:16:05 +0530

    conversion of dataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+let temp = null
+
+//  console.log(typeof score)
+//  console.log(typeof (score))
+
+let valueInNumber = Number(score) 
+
+//  console.log(typeof valueInNumber)
+//  console.log(valueInNumber)
+
+//  console.log(temp)
+//  console.log(typeof temp)
+
+let isLoggedIn = 1
+//  console.log(typeof isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(typeof booleanIsLoggedIn)
+//  console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+//  console.log(typeof someNumber)
+let stringNumber = String(someNumber)
+//  console.log(typeof stringNumber)
